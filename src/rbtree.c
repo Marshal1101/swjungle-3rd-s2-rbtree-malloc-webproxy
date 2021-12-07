b0c4d3687e52df953abdd79a03b011c5e7a43aa7
Marshal1101 <adequateplayer9@gmail.com>
2021-12-07 14:09:49 +0900

    segment error 83line z->parent->color not exist

--- src/rbtree.c
+++ src/rbtree.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 rbtree *new_rbtree(void) {
-  rbtree *p
-   = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
-  p->nil = (node_t *)calloc(1, sizeof(node_t));
-  p->nil->color = RBTREE_BLACK;
-  p->root = p->nil;
+  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+  node_t *nil = (node_t *)calloc(1, sizeof(node_t));
+  nil->color = RBTREE_BLACK;
+  p->nil = nil;
+  p->root = nil;
+  p->root->parent = nil;
   return p;
 }
 
@@ -24,7 +24,6 @@ void delete_node(rbtree *t, node_t *x) {
 
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
-  
   delete_node(t, t->root);
   free(t->nil);
   free(t);
@@ -57,7 +56,7 @@ void left_rotate(rbtree *t, node_t *x) {
 
 // 추가된 함수
 void right_rotate(rbtree *t, node_t *y) {
-// 트리 받고, x를 받는다. x는 FIXUP에서 이미 부모로 설정되고 들어온 z의 부모이다.
+// 트리 받고, y를 받는다. y는 FIXUP에서 이미 부모로 설정되고 들어온 z의 부모이다.
 // CLRS 책과 통일하기 위해 y, x, z 매개변수 설정
 // right_rotate는 left_rotate 코드와 대칭
   node_t *x = y->left;
@@ -69,11 +68,11 @@ void right_rotate(rbtree *t, node_t *y) {
   if (y->parent == t->nil) {
     t->root = x;
   }
-  else if (y == y->parent->left) {
-    y->parent->left = x;
+  else if (y == y->parent->right) {
+    y->parent->right = x;
   }
   else {
-    y->parent->right = x;
+    y->parent->left = x;
   }
   x->right = y;
   y->parent = x;
@@ -143,7 +142,6 @@ node_t *rbtree_insert_fixup(rbtree *t, node_t *z) {
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
   node_t *z = (node_t *)calloc(1, sizeof(node_t));    // z는 새로운 노드
   z->key = key;
   node_t *y = t->nil;
@@ -170,7 +168,6 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
   }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
   node_t *n = t->root;
   while (n != t->nil && key != n->key) {
     if (key < n->key)
@@ -186,7 +183,6 @@ node_t *rbtree_find(const rbtree *t, const key_t key) {
 }
 
 node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
   node_t *ptr = t->root;
   if (ptr == t->nil)
     return ptr;
@@ -197,7 +193,6 @@ node_t *rbtree_min(const rbtree *t) {
 }
 
 node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
   node_t *ptr = t->root;
   if (ptr == t->nil)
     return ptr;
@@ -219,7 +214,7 @@ void rbtree_transplant(rbtree *t, node_t *u, node_t *v) {
 
 // 추가된 함수
 void rbtree_erase_fixup(rbtree *t, node_t *x) {
-// *x 삭제 대상 포인터, x의 형제를 가리키는 포인터
+// *x 삭제 대상 포인터, w는 x의 형제를 가리키는 포인터
   node_t *w;
   while (x!= t->root && x->color == RBTREE_BLACK) {
     if (x == x->parent->left) {
@@ -279,7 +274,6 @@ x->color = RBTREE_BLACK;
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
-  // TODO: implement erase
   // 여기서 p는 CLRS에서 나오는 z ; 삭제 대상
   node_t *y = p;
   color_t y_origin_color = y->color;
@@ -322,13 +316,13 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   // TODO: implement to_array
   return 0;
 }
-// int main(){
-//   rbtree *t = new_rbtree();
-//   rbtree_insert(t, 1);
-//   rbtree_insert(t, 2);
-//   rbtree_insert(t, 3);
-//   rbtree_insert(t, 4);
-//   rbtree_min(t);
-//   rbtree_max(t);
-//   return 0;
-// }
+int main(){
+  rbtree *t = new_rbtree();
+  rbtree_insert(t, 5);
+  rbtree_insert(t, 4);
+  rbtree_insert(t, 3);
+  rbtree_insert(t, 2);
+  rbtree_min(t);
+  rbtree_max(t);
+  return 0;
+}
